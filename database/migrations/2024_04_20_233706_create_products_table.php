2c468c2591113f0168a36f8c26873750d86588e2
yuma11120 <gameyuma111@gmail.com>
2024-04-24 22:24:46 +0900

    テーブル作成の変更点修正

--- database/migrations/2024_04_20_233706_create_products_table.php
+++ database/migrations/2024_04_20_233706_create_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
      * @return void
      */
     public function up()
-    {
+    {//テーブル作成時にはコピペ
         Schema::create('products', function (Blueprint $table) {
             $table->id('id');
             $table->bigInteger('company_id');
@@ -21,6 +21,20 @@ return new class extends Migration
             $table->bigInteger('stock');
             $table->text('comment')->nullable();
             $table->string('img_path')->nullable();
+            $table->timestamps(); 
+        });
+
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('company_name');
+            $table->string('street_address')->nullable();
+            $table->string('representative_name')->nullable();
+            $table->timestamps(); 
+        });
+
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id('id');
+            $table->bigInteger('product_id');
             $table->timestamps(); 
         });
     }
